37f2f825f5db937f4284b3e0c2ce1b18aba047d2
Hassan <106278943+hassan-ahmed12@users.noreply.github.com>
2024-11-18 00:52:54 +0500

    dev

---
+++ assets/custom-script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Video Slider Initialization
+  const swiper = new Swiper(".video-carousel", {
+    spaceBetween: 10,
+    draggable: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 1,
+      },
+      750: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
